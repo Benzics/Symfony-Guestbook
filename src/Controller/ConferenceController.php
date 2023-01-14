996ea4e34d933e07eb4d094aa25cd817d9e35f22
benzics <benjaminnicholas29@gmail.com>
2023-01-14 09:33:55 +0100

    conference view page and simple listing done

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,22 +2,26 @@
 
 namespace App\Controller;
 
+use App\Repository\ConferenceRepository;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Conference;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-        return new Response(<<<EOF
-            <html>
-                <body>
-                    <img src="/images/under-construction.gif" />
-                </body>
-            </html>
-            EOF
-    );
+        return $this->render('conference/index.html.twig', ['conferences' => $conferenceRepository->findAll()]);
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Conference $conference, CommentRepository $commentRepository)
+    {
+        $comments = $commentRepository->findBy(['conference' => $conference]);
+
+        return $this->render('conference/show.html.twig', compact('conference', 'comments'));
     }
 }
